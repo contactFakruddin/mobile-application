822c455acb6ff8d35f49bed33d8ce04e4ad06126
Fakruddin <167856209+contactFakruddin@users.noreply.github.com>
2024-11-08 23:34:19 +0530

    Update practical4.dart

--- 22000518_hybrid_mobile_application_practical - Copy/hybrid_mobile_application_practicals/practical4.dart
+++ 22000518_hybrid_mobile_application_practical - Copy/hybrid_mobile_application_practicals/practical4.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-              Image.asset('assets/nuvLogo.png'),
+              Image.asset('assets/NUVimage.png'),
               Text('Welcome to NUV',
                 style: TextStyle(fontSize: 50.0),
               )
